9c5a88330d187b53382ea0e3130e1c0c492257b1
Mariano <macacozzz@hotmail.com>
2022-11-17 18:21:49 -0300

    animaciones de gsap y repaso de useRef

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react"
+import { useEffect, useLayoutEffect, useRef, useState } from "react"
+import { gsap } from 'gsap';
 
 const ERROR_STYLE = 'color: red; background-color: black;';
 const MAXIMUN_COUNT = 10;
@@ -7,19 +8,29 @@ export const CounterEffect = ()   => {
 
   const [counter, setCounter] = useState(5)
 
+  const counterElement = useRef<HTMLHeadingElement>(null); //no re-renderiza el elemento cuando cambia counterElement
+
   const handleClick = () => {
     setCounter( prev => Math.min( prev +1 , MAXIMUN_COUNT) );
   }
 
-  useEffect(() => {
+  useLayoutEffect(() => { // layoutEff se ejecuta despues de que se cargo el html
     if( counter < MAXIMUN_COUNT ) return;
     console.log('%cSe llego al valor maximo', ERROR_STYLE );
+
+    const tl = gsap.timeline();
+
+    tl.to( counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out'} )
+      .to( counterElement.current, { y: 0, duration: 1, ease: 'bounce.out'})
+
+
   }, [counter])
   
 
   return (
     <div>
-        <h1> Counter Effect : { counter }</h1>
+        <h1> Counter Effect:</h1>
+        <h2 ref={ counterElement }>{ counter }</h2>
         <button
             onClick={ handleClick }
         >
